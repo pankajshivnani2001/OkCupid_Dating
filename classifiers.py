d15241d45ab4931619f7ca6d274b7335df23738a
pankajshivnani2001 <67276848+pankajshivnani2001@users.noreply.github.com>
2022-02-20 14:15:41 +0530

    Add Naive Bayes Classifier

--- classifiers.py
+++ classifiers.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 def app():
     df = pd.read_csv("nb.csv")
@@ -16,7 +17,15 @@ def app():
 
     tf_idf_vec = TfidfVectorizer()
     tf_idf = tf_idf_vec.fit_transform(X_train)
-    st.write("Some Features(aka words) and their Tf-Idf score:")
-    st.write(tf_idf_vec.get_feature_names_out()[:20])
-    st.write(tf_idf_vec.get_feature_names_out()[-20:])
-    st.write(tf_idf_vec.get_feature_names_out()[7000:7020])
+    st.subheader("Features(aka Words) and their Tf-Idf score:")
+    st.write(pd.DataFrame(tf_idf.toarray(),columns=tf_idf_vec.get_feature_names()))
+
+    nb_clf = MultinomialNB()
+    nb_clf.fit(tf_idf, y_train)
+
+    st.subheader("Naive Bayes Predictions vs Original Gender")
+    predictions = nb_clf.predict(tf_idf_vec.transform(X_test))
+    st.write(pd.DataFrame({"Predictions":predictions, "Original":y_test}))
+    
+    st.subheader("Performance")
+    #bar chart for accuracy precision recall
